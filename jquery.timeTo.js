28d2eeedc2fe753f2535d823f035c3616c3ef285
Alexey Teterin <altmoc@gmail.com>
2015-02-22 23:47:01 +0200

    reset has been fixed

--- jquery.timeTo.js
+++ jquery.timeTo.js
@@ -3,10 +3,12 @@
  * Show countdown timer or realtime clock
  *
  * @author Alexey Teterin <altmoc@gmail.com>
- * @version 1.0.13
+ * @version 1.0.15
  * @license MIT http://opensource.org/licenses/MIT
  * @date 2014-01-17
  */
+'use strict';
+
 (function (factory) {
     if (typeof exports === 'object') {
         // CommonJS (Node)
@@ -21,8 +23,31 @@
     }
 }(function ($) {
 
+    var defaults = {
+        callback: null,          // callback function for exec when timer out
+        captionSize: 0,          // font-size by pixels for captions, if 0 then calculate automaticaly
+        countdown: true,         // is countdown or real clock
+        countdownAlertLimit: 10, // limit in seconds when display red background
+        displayCaptions: false,  // display captions under digit groups
+        displayDays: 0,          // display day timer, count of days digits
+        displayHours: true,      // display hours
+        fontFamily: 'Verdana, sans-serif',
+        fontSize: 28,            // font-size of a digit by pixels
+        lang: 'en',              // language of caption
+        seconds: 0,              // timer's countdown value in seconds
+        start: true,             // true to start timer immediately
+        theme: 'white',          // 'white' or 'black' theme fo timer's view
+        
+        vals: [0, 0, 0, 0, 0, 0, 0, 0, 0],  // private, current value of each digit
+        limits: [9, 9, 9, 2, 9, 5, 9, 5, 9],// private, max value of each digit
+        iSec: 8,            // private, index of second digit
+        iHour: 4,           // private, index of hour digit
+        tickTimeout: 1000,  // timeout betweet each timer tick in miliseconds
+        intervalId: null    // private
+    };
+
     var methods = {
-        start: function(sec){
+        start: function(sec) {
             if(sec) init.call(this, sec);
             var me = this,
                 intervalId = setTimeout(function(){ tick.call(me); }, 1000);
@@ -32,7 +57,7 @@
             this.data('intervalId', intervalId);
         },
 
-        stop: function(){
+        stop: function() {
             var data = this.data();
 
             if(data.intervalId){
@@ -49,26 +74,24 @@
             this.find('ul').parent().removeClass('timeTo-alert');
 
             if(typeof sec === "undefined") { sec = data.value; }
-            if (data.vals) { data.vals = null; }
-            init.call(this, sec);
+            init.call(this, sec, true);
         }
+    };
 
-    };
-    
     var dictionary = {
-        en:{days:"days", hours:"hours", min:"minutes", sec:"seconds"},
-        ru:{days:"дней", hours:"часов", min:"минут", sec:"секунд"},
-        ua:{days:"днiв", hours:"годин", min:"хвилин", sec:"секунд"},
-        de:{days:"Tag", hours:"Uhr", min:"Minuten", sec:"Secunden"},
-        fr:{days:"jours", hours:"heures", min:"minutes", sec:"secondes"},
-        sp:{days:"días", hours:"reloj", min:"minutos", sec:"segundos"},
-        it:{days:"giorni", hours:"ore", min:"minuti", sec:"secondi"},
-        nl:{days:"dagen", hours:"uren", min:"minuten", sec:"seconden"},
-        no:{days:"dager", hours:"timer", min:"minutter", sec:"sekunder"},
-        pt:{days:"dias", hours:"horas", min:"minutos", sec:"segundos"}
+        en:{days:'days',   hours:'hours',  min:'minutes',  sec:'seconds'},
+        ru:{days:'дней',   hours:'часов',  min:'минут',    sec:'секунд'},
+        ua:{days:'днiв',   hours:'годин',  min:'хвилин',   sec:'секунд'},
+        de:{days:'Tag',    hours:'Uhr',    min:'Minuten',  sec:'Secunden'},
+        fr:{days:'jours',  hours:'heures', min:'minutes',  sec:'secondes'},
+        sp:{days:'días',   hours:'reloj',  min:'minutos',  sec:'segundos'},
+        it:{days:'giorni', hours:'ore',    min:'minuti',   sec:'secondi'},
+        nl:{days:'dagen',  hours:'uren',   min:'minuten',  sec:'seconden'},
+        no:{days:'dager',  hours:'timer',  min:'minutter', sec:'sekunder'},
+        pt:{days:'dias',   hours:'horas',  min:'minutos',  sec:'segundos'}
     };
     
-    if(typeof $.support.transition === 'undefined'){
+    if(typeof $.support.transition === 'undefined') {
         $.support.transition = (function(){
             var thisBody = document.body || document.documentElement,
                 thisStyle = thisBody.style,
@@ -77,80 +100,66 @@
             return support;
         })();
     }
-    
-    $.fn.timeTo = function(){
-        var defaults = {
-                callback: null,          // callback function for exec when timer out
-                captionSize: 0,          // font-size by pixels for captions, if 0 then calculate automaticaly
-                countdown: true,         // is countdown or real clock
-                countdownAlertLimit: 10, // limit in seconds when display red background
-                displayCaptions: false,  // display captions under digit groups
-                displayDays: 0,          // display day timer, count of days digits
-                displayHours: true,      // display hours
-                fontFamily: "Verdana, sans-serif",
-                fontSize: 28,            // font-size of a digit by pixels
-                lang: 'en',              // language of caption
-                seconds: 0,              // timer's countdown value in seconds
-                start: true,             // true to start timer immediately
-                theme: "white",          // 'white' or 'black' theme fo timer's view
-                
-                vals: [0, 0, 0, 0, 0, 0, 0, 0, 0],  // private, current value of each digit
-                limits: [9, 9, 9, 2, 9, 5, 9, 5, 9],// private, max value of each digit
-                iSec: 8,            // private, index of second digit
-                iHour: 4,           // private, index of hour digit
-                tickTimeout: 1000,  // timeout betweet each timer tick in miliseconds
-                intervalId: null    // private
-            },
-            method, options = {};
 
-        for(var i = 0, arg; arg = arguments[i]; ++i){
-            if(i == 0 && typeof arg === "string"){
+
+    $.fn.timeTo = function(){
+        var method, options = {};
+
+        for(var i = 0, arg; arg = arguments[i]; ++i) {
+            if(i == 0 && typeof arg === 'string') {
                 method = arg;
-            }else{
-                if(typeof arg == "object"){
-                    if(typeof arg.getTime === 'function'){
+            }
+            else {
+                if(typeof arg === 'object') {
+                    if(typeof arg.getTime === 'function') {
                         options.timeTo = arg;
-                    }else {
+                    }
+                    else {
                         options = $.extend(options, arg);
                     }
-                }else{
-                    if(typeof arg == "function"){
+                }
+                else {
+                    if(typeof arg === 'function') {
                         options.callback = arg;
-                    }else{
-                        var v = parseInt(arg);
-                        if(!isNaN(v)){
+                    }
+                    else {
+                        var v = parseInt(arg, 10);
+                        if(!isNaN(v)) {
                             options.seconds = v;
                         }
                     }
                 }
             }
         }
+
         // set time for countdown to
-        if(options.timeTo){
+        if(options.timeTo) {
             var time,
                 now = (new Date()).getTime();
 
-            if(options.timeTo.getTime){ // set time as date object
+            if(options.timeTo.getTime) { // set time as date object
                 time = options.timeTo.getTime();
-            }else if(typeof options.timeTo === 'number'){  // set time as integer in millisec
+            }
+            else if(typeof options.timeTo === 'number') {  // set time as integer in millisec
                 time = options.timeTo;
             }
-            if(options.timeTo > now){
+            if(options.timeTo > now) {
                 options.seconds = Math.floor((time - now) / 1000);
             }
-        }else if(options.time || !options.seconds){
+        }else if(options.time || !options.seconds) {
             var time = options.time;
 
             if(!time) time = new Date();
 
-            if(typeof time === 'object' && time.getTime){
+            if(typeof time === 'object' && time.getTime) {
                 options.seconds = time.getHours()*3600 + time.getMinutes()*60 + time.getSeconds();
                 options.countdown = false;
-            }else if(typeof time === 'string'){
+            }
+            else if(typeof time === 'string') {
                 var tt = time.split(':'),
                     sec = 0, m = 1, t;
 
-                while(t = tt.pop()){
+                while(t = tt.pop()) {
                     sec += t*m;
                     m *= 60;
                 }
@@ -158,26 +167,40 @@
                 options.countdown = false;
             }
         }
+
         if(options.countdown !== false && options.seconds > 86400 && typeof options.displayDays === 'undefined') {
             var days = Math.floor(options.seconds / 86400);
             options.displayDays = days < 10 && 1 || days < 100 && 2 || 3;
-        }else if(options.displayDays === true) {
+        }
+        else if(options.displayDays === true) {
             options.displayDays = 3;
-        }else if(options.displayDays) {
+        }
+        else if(options.displayDays) {
             options.displayDays = options.displayDays > 0 ? Math.floor(options.displayDays) : 3;
         }
 
-        
-        return this.each(function(){
+        return this.each(function() {
             var $this = $(this),
                 data = $this.data(),
                 i;
 
-            if(!data.vals){ // new clock
+
+            if(data.intervalId) {
+                clearInterval(data.intervalId);
+                data.intervalId = null;
+            }
+
+            if(!data.vals || method === 'reset') { // new clock
+                if(data.options) {
+                    options = data.options;
+                }
                 data = $.extend(defaults, options);
-                data.height = Math.round(data.fontSize*100/93);
-                data.width = Math.round(data.fontSize*.8 + data.height*.13);
+                data.options = options;
+
+                data.height = Math.round(data.fontSize * 100 / 93);
+                data.width = Math.round(data.fontSize * .8 + data.height * .13);
                 data.displayHours = !!(data.displayDays || data.displayHours);
+
                 $this
                     .addClass('timeTo')
                     .addClass('timeTo-'+ data.theme)
@@ -193,7 +216,8 @@
                     dhtml2 = '<div'+ style +'>'+ ulhtml,
                     dot2 = '<span>:</span>',
                     maxWidth = Math.round(data.width * 2 + 3),
-                    captionSize = data.captionSize || Math.round(data.fontSize * 0.43);
+                    captionSize = data.captionSize || Math.round(data.fontSize * 0.43),
+
                     thtml = (data.displayCaptions ?
                         (data.displayHours
                             ? '<figure style="max-width:'+ maxWidth +'px">$1<figcaption style="font-size:'+ captionSize +'px">'+ dictionary[data.lang].hours +'</figcaption></figure>'+ dot2
@@ -203,7 +227,7 @@
                         : (data.displayHours ? '$1'+ dot2 : '') +'$1'+ dot2 +'$1'
                     ).replace(/\$1/g, dhtml1 + dhtml2);
 
-                if(data.displayDays > 0){
+                if(data.displayDays > 0) {
                     var marginRight = data.fontSize * 0.4,
                         dhtml = dhtml1;
                     for(i = data.displayDays - 1; i > 0; i--) {
@@ -216,17 +240,14 @@
                         ) + thtml;
                 }
                 $this.html(thtml);
-            }else{ // exists clock
-                if(data.intervalId){
-                    clearInterval(data.intervalId);
-                    data.intervalId = null;
-                }
+            }
+            else { // exists clock
                 $.extend(data, options);
             }
             
             var $digits = $this.find('div');
 
-            if($digits.length < data.vals.length){
+            if($digits.length < data.vals.length) {
                 var dif = data.vals.length - $digits.length,
                     vals = data.vals, limits = data.limits;
 
@@ -242,25 +263,32 @@
             data.sec = data.seconds;
             $this.data(data);
             
-            if(method && methods[method]){
+            if(method && methods[method]) {
                 methods[ method ].call($this, data.seconds);
-            }else if(data.start){
+            }
+            else if(data.start) {
                 methods.start.call($this, data.seconds);
-            }else {
+            }
+            else {
                 init.call($this, data.seconds);
             }
         });
     };
 
-    function init(sec){
+
+    function init(sec, force) {
         var data = this.data(),
-            $digits = this.find('ul');
+            $digits = this.find('ul'),
+            isInterval = false;
 
-        if (!data.vals || $digits.length === 0) return;
+        if (!data.vals || $digits.length === 0) {
+            return;
+        }
 
-        if(!sec) sec = data.seconds;
+        if(!sec) {
+            sec = data.seconds;
+        }
 
-        var isInterval = false;
         if (data.intervalId) {
             isInterval = true;
             clearTimeout(data.intervalId);
@@ -279,12 +307,12 @@
         var s = sec - rest,
             str = (days < 100 ? '0' + (days < 10 ? '0' : '') : '') + days + (h < 10 ? '0' : '') + h + (m < 10 ? '0' : '') + m + (s < 10 ? '0' : '') + s;
 
-        for (var i = data.vals.length - 1, j = str.length - 1, v; i >= 0; i--, j--) {
+        for(var i = data.vals.length - 1, j = str.length - 1, v; i >= 0; i--, j--) {
             v = parseInt(str.substr(j, 1));
             data.vals[i] = v;
             $digits.eq(i).children().html(v);
         }
-        if (isInterval) {
+        if(isInterval || force) {
             var me = this;
             data.ttStartTime = Date.now();
             data.intervalId = setTimeout(function(){ tick.call(me); }, 1000);
@@ -300,16 +328,18 @@
         var $digits = this.find('ul'),
             data = this.data();
 
-        if (!data.vals || $digits.length == 0){
-            if(data.intervalId){
+        if(!data.vals || $digits.length == 0) {
+            if(data.intervalId) {
                 clearTimeout(data.intervalId);
                 this.data('intervalId', null);
             }
-            if(data.callback) data.callback();
+            if(data.callback) {
+                data.callback();
+            }
 
             return;
         }
-        if (digit == undefined) {
+        if(digit == undefined) {
             digit = data.iSec;
         }
 
@@ -321,7 +351,7 @@
         $li.eq(1).html(n);
         n += step;
 
-        if(digit == data.iSec){
+        if(digit == data.iSec) {
             var tickTimeout = data.tickTimeout,
                 timeDiff = (new Date()).getTime() - data.ttStartTime;
 
@@ -333,15 +363,16 @@
         }
         
         if(n < 0 || n > data.limits[digit]) {
-            if(n < 0)
-            {
+            if(n < 0) {
                 n = data.limits[digit];
                 if(digit == data.iHour && data.displayDays > 0 && digit > 0 && data.vals[digit-1] == 0) // fix for hours when day changing
                     n = 3;
             }
-            else 
+            else {
                 n = 0;
-            if(digit > 0){
+            }
+
+            if(digit > 0) {
                 tick.call(this, digit-1);
             }
         }
@@ -351,48 +382,59 @@
         
         var me = this;
         
-        if($.support.transition){
+        if($.support.transition) {
             $ul.addClass('transition');
             $ul.css({top:0});
 
-            setTimeout(function(){
+            setTimeout(function() {
                 $ul.removeClass('transition');
                 $li.eq(1).html(n);
                 $ul.css({top:"-"+ data.height +"px"});
 
-                if(step > 0 || digit != data.iSec) return;
+                if(step > 0 || digit != data.iSec) {
+                    return;
+                }
 
-                if(data.sec == data.countdownAlertLimit){
+                if(data.sec == data.countdownAlertLimit) {
                     $digits.parent().addClass('timeTo-alert');
                 }
-                if(data.sec === 0){
+
+                if(data.sec === 0) {
                     $digits.parent().removeClass('timeTo-alert');
 
-                    if(data.intervalId){
+                    if(data.intervalId) {
                         clearTimeout(data.intervalId);
                         me.data('intervalId', null);
                     }
 
-                    if(typeof data.callback === 'function') data.callback();
+                    if(typeof data.callback === 'function') {
+                        data.callback();
+                    }
                 }
             }, 410);
-        }else{
-            $ul.stop().animate({top:0}, 400, digit != data.iSec ? null : function(){
+        }
+        else {
+            $ul.stop().animate({top:0}, 400, digit != data.iSec ? null : function() {
                 $li.eq(1).html(n);
                 $ul.css({top:"-"+ data.height +"px"});
-                if(step > 0 || digit != data.iSec) return;
+                if(step > 0 || digit != data.iSec) {
+                    return;
+                }
 
-                if(data.sec == data.countdownAlertLimit){
+                if(data.sec == data.countdownAlertLimit) {
                     $digits.parent().addClass('timeTo-alert');
-                }else if(data.sec == 0){
+                }
+                else if(data.sec == 0) {
                     $digits.parent().removeClass('timeTo-alert');
 
-                    if(data.intervalId){
+                    if(data.intervalId) {
                         clearTimeout(data.intervalId);
                         me.data('intervalId', null);
                     }
 
-                    if(typeof data.callback === 'function') data.callback();
+                    if(typeof data.callback === 'function') {
+                        data.callback();
+                    }
                 }
             });
         }
